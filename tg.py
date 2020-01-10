e9a32ab6e3a7c56f1a28f8f9316a5c6fe8922bf0
djeck1432 <djeck1432@users.noreply.github.com>
2020-01-10 11:11:23 +0100

    get out chat id from code

--- tg.py
+++ tg.py
@@ -3,21 +3,22 @@ import os
 import telegram
 import argparse
 
-load_dotenv()
-telegram_token = os.getenv('TELEGRAM_TOKEN')
-bot = telegram.Bot(token=telegram_token)
 
-def post_telegram(image_url,text):
-    bot.send_message(chat_id='@test_djeck1432',text=text)
-    bot.send_photo(chat_id='@test_djeck1432',photo=open(image_url, 'rb'))
+def post_telegram(image_url, text):
+    bot.send_message(chat_id=telegram_chat_id, text=text)
+    with open(image_url, 'rb') as photo:
+        bot.send_photo(chat_id=telegram_chat_id, photo=photo)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     load_dotenv()
     telegram_token = os.getenv('TELEGRAM_TOKEN')
+    telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
     bot = telegram.Bot(token=telegram_token)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('image_url', help='Введите ссылку на изображения')
+    parser.add_argument('image_path', help='Введите ссылку на изображения')
     parser.add_argument('text', help='Введите текст к изображению')
     args = parser.parse_args()
-    post_telegram(args.image_url, args.text)
+    post_telegram(args.image_path, args.text)
+
