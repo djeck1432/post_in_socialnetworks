8023ccd0c0fc06fb017be6a541bfdc041aed231a
djeck1432 <djeck1432@users.noreply.github.com>
2020-01-10 11:12:55 +0100

    get out owner_id and album_id from code

--- vkontakte.py
+++ vkontakte.py
@@ -4,34 +4,31 @@ import os
 import argparse
 
 
-
-
-def post_vkontakte(url_image, text):
-    vk_session = vk_api.VkApi(vk_phone,vk_password)
+def post_vkontakte(image_path, text):
+    vk_session = vk_api.VkApi(vk_phone, vk_password)
     vk = vk_session.get_api()
     vk_session.auth()
-    owner_id = -190053871
     upload = vk_api.VkUpload(vk_session)
+    owner_id = '-' + vk_owner_id
     photo = upload.photo(
-            url_image,
-            group_id =190053871,
-            album_id=269466802
-        )
-    vk_photo_url = 'https://vk.com/photo{}_{}'.format(
-    photo[0]['owner_id'], photo[0]['id']
+        image_path,
+        group_id=vk_owner_id,
+        album_id=vk_album_id
     )
-    attachment_photo = 'photo{}_{}'.format(photo[0]['owner_id'],photo[0]['id'])
-    print(vk.wall.post(owner_id=owner_id, message=text, attachment=attachment_photo))
+    attachment_photo = 'photo{}_{}'.format(photo[0]['owner_id'], photo[0]['id'])
+    return vk.wall.post(owner_id=owner_id, message=text, attachment=attachment_photo)
+
 
 if __name__ == '__main__':
     load_dotenv()
     vk_token = os.getenv('VK_TOKEN')
     vk_phone = os.getenv('VK_PHONE')
     vk_password = os.getenv('VK_PASSWORD')
+    vk_owner_id = os.getenv('VK_OWNER_ID')
+    vk_album_id = os.getenv('VK_ALBUM_ID')
 
     parser = argparse.ArgumentParser(description='Введите url-изображения и текст')
-    parser.add_argument('url_image', help='Введите url-изображения')
+    parser.add_argument('image_path', help='Введите url-изображения')
     parser.add_argument('text', help='Введите текст')
     args = parser.parse_args()
-    post_vkontakte(args.url_image, args.text)
-
+    post_vkontakte(args.image_path, args.text)
